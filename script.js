2f95cb9058588dabb6c58631796558a883be251b
Vennela6281 <238x1a4515@khitguntur.ac.in>
2026-02-25 07:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to("#car", {
-  x: "300%",
-  ease: "none",
+  x: "350%",
+  ease: "power1.out",
   scrollTrigger: {
     trigger: ".hero",
     start: "top top",
     end: "+=1500",
-    scrub: true,
+    scrub: 1,
     pin: true
   }
 });
